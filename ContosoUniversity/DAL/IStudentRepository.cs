12a8b0eb828ac55707d4d63ea19475b2549f6eeb
iplusplus812 <iplusplus@yeah.net>
2014-11-29 01:27:46 +0800

    add ddd with repository and unit of work support  to branch repo-unitoofwork-model

---
+++ ContosoUniversity/DAL/IStudentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.DAL
+{
+    public interface IStudentRepository:IDisposable
+    {
+        IEnumerable<Student> GetStudents();
+        Student GetStudentById(int studentId);
+        void InsertStudent(Student student);
+        void DeleteStudent(int studentId);
+        void UpdateStudent(Student student);
+        void Save();
+    }
+}
